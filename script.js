45372db60d71e7dbf08fe4f8e0159eaec728f7d8
nmkisonline <113755178+nmkisonline@users.noreply.github.com>
2023-10-07 00:43:16 -0400

    added preset events

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ let countrySelector = document.getElementById('country-selector');
 let channelSelector = document.getElementById('channel-selector');
 let dateSelector = document.getElementById('date-selector');
 let timeSelector = document.getElementById('time-selector');
+let eventSelector = document.getElementById('event-selector');
 let currentTimeDisplay = document.getElementById('current-time-display');
 let totalTimeDisplay = document.getElementById('total-time-display');
 
 let channels = {
-    usa: [
-        {name: 'Channel 1 USA', url: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'},
-        {name: 'Channel 2 USA', url: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'}
-    ],
-    uk: [
-        {name: 'Channel 1 UK', url: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'},
-        {name: 'Channel 2 UK', url: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'}
-    ]
+    usa: Array.from({length: 10}, (_, i) => ({name: `USA Channel ${i+1}`, url: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'})),
+    uk: Array.from({length: 10}, (_, i) => ({name: `UK Channel ${i+1}`, url: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'})),
+    india: Array.from({length: 10}, (_, i) => ({name: `India Channel ${i+1}`, url: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'}))
+};
+
+let historicalEvents = {
+    '911': {country: 'usa', date: '2001-09-11', time: 0},
+    '2012': {country: 'usa', date: '2012-12-21', time: 0},
+    '1947': {country: 'india', date: '1947-08-15', time: 0}
 };
 
 countrySelector.addEventListener('change', loadChannels);
 channelSelector.addEventListener('change', playChannel);
 dateSelector.addEventListener('change', playChannel);
 timeSelector.addEventListener('input', seekVideo);
+eventSelector.addEventListener('change', selectHistoricalEvent);
 
 video.addEventListener('loadedmetadata', updateTotalTime);
 video.addEventListener('timeupdate', updateTimeDisplay);
@@ -58,4 +61,16 @@ function updateTimeDisplay() {
     let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60);
     currentTimeDisplay.innerText = ('0' + currentMinutes).slice(-2) + ':' + ('0' + currentSeconds).slice(-2);
     timeSelector.value = video.currentTime;
+}
+
+function selectHistoricalEvent() {
+    let eventKey = eventSelector.value;
+    if (historicalEvents[eventKey]) {
+        let event = historicalEvents[eventKey];
+        countrySelector.value = event.country;
+        loadChannels();
+        dateSelector.value = event.date;
+        timeSelector.value = event.time;
+        seekVideo();
+    }
 }
